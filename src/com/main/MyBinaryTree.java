73b1c196edb3c1537c7cade490fa1cfb52e9d850
dbasumatary <basudhondip@gmail.com>
2023-03-14 16:06:39 +0530

    Refactored: Program to create a Binary Search Tree of multiple nodes

--- src/com/main/MyBinaryTree.java
+++ src/com/main/MyBinaryTree.java
@@ -66,3 +66,4 @@ public class MyBinaryTree<K extends Comparable<K>>{
         return "MyBinaryTree{" + root + '}';
     }
 }
+
--- src/com/main/MyBinaryTreeMain.java
+++ src/com/main/MyBinaryTreeMain.java
@@ -6,7 +6,23 @@ public class MyBinaryTreeMain {
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+
 
         myBinaryTree.printBinaryTree();
+        //myBinaryTree.printInOrderWithLevel(myBinaryTree.getRoot(), 0);
+        //myBinaryTree.printTreeDiagramLevelOrder();
+
+        System.out.println();
+        System.out.println("The size of the Binary Search Tree is : " + myBinaryTree.getSize());
     }
 }
